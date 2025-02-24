30a030d148502983a27102997b2a6b9b732f83af
lukkasmatheu <57408709+lukkasmatheu@users.noreply.github.com>
2025-02-23 22:40:22 -0300

    Update UserCreationRequest.java

--- src/main/java/com/financial/self/models/request/UserCreationRequest.java
+++ src/main/java/com/financial/self/models/request/UserCreationRequest.java
@@ -27,7 +27,7 @@ public class UserCreationRequest {
     private String email;
 
     @NotBlank(message = "Password must not be empty")
-    @Size(min = 6, message = "Password length must be at least 6 characters long")
+    @Size(min = 5, message = "Password length must be at least 6 characters long")
     @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "Secure password to enable user login", example = "somethingSecure")
     private String password;
 
